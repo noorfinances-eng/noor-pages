7005f83e328338c0ce7dd62302d97af1e234778a
noorfinances-eng <noorfinances@gmail.com>
2025-11-05 23:10:47 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: { gold: "#FFD700", night: "#0a0a0a" }
+    }
+  },
+  plugins: []
+};
